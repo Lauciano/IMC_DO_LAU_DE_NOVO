f60fd9690ea3266833cbbf3d4f2d8ef16a6f9186
lauciano <jose.lauciano@gmail.com>
2018-03-05 20:54:21 -0300

    - Funcionalidades da Tela Principal Implementadas --- Calcular IMC a partir de Peso e Altura

--- app/src/main/java/com/lau/laucianofa/imc_do_lau/MainActivity.java
+++ app/src/main/java/com/lau/laucianofa/imc_do_lau/MainActivity.java
@@ -2,12 +2,39 @@ package com.lau.laucianofa.imc_do_lau;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText peso, altura;
+    TextView resultado;
+    Button calcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        peso = (EditText) findViewById(R.id.pesoEditText);
+        altura = (EditText) findViewById(R.id.alturaEditText);
+        resultado = (TextView) findViewById(R.id.resultadoTextView);
+        calcular = (Button) findViewById(R.id.calcularButton);
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                double imc, ps, al;
+                // ps recebe peso em kg
+                ps = Double.parseDouble(peso.getText().toString());
+                // al recebe altura em m
+                al = Double.parseDouble(altura.getText().toString()) / 100;
+                // imc recebe calculo do imc
+                imc = ps / (al*al);
+                resultado.setText("Seu IMC é " + ((double) Math.round(imc*10))/10);
+            }
+        });
     }
 }

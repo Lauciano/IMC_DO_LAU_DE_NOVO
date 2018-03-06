fd13f47ea0ccc0c7eefdce1bc305d134b4f309f1
lauciano <jose.lauciano@gmail.com>
2018-03-05 23:26:39 -0300

    - Melhorada Tela Principal -- Adicionado título -- Adicionado resultado -- Melhor centralizada região ativa -- Teclado virtual desaparece ao pressionar botão - Funcionalidades da tela principal foram modificadas para funcionar ++ com as modificações

--- app/src/main/java/com/lau/laucianofa/imc_do_lau/MainActivity.java
+++ app/src/main/java/com/lau/laucianofa/imc_do_lau/MainActivity.java
@@ -1,12 +1,16 @@
 package com.lau.laucianofa.imc_do_lau;
 
+import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     AlertDialog.Builder builder;
     Button calcular;
     EditText peso, altura;
-    TextView resultado;
+    TextView alerta, resultado;
     View popup;
 
     @Override
@@ -26,17 +30,26 @@ public class MainActivity extends AppCompatActivity {
         peso = (EditText) findViewById(R.id.pesoEditText);
         altura = (EditText) findViewById(R.id.alturaEditText);
         calcular = (Button) findViewById(R.id.calcularButton);
+        resultado = (TextView) findViewById(R.id.resultadoTextView);
 
         calcular.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Definindo elementos de popup
-                builder = new AlertDialog.Builder(MainActivity.this);
-                popup = getLayoutInflater().inflate(R.layout.result_popup, null);
-                resultado = (TextView) popup.findViewById(R.id.resultadoTextView);
+                // Escondendo teclado virtual
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
 
                 // Checando se usuário digitou peso e altura
                 if(peso.getText().toString().isEmpty() || altura.getText().toString().isEmpty()) {
-                    resultado.setText("Por favor, insira peso e altura.");
+                    // Definindo elementos de popup
+                    builder = new AlertDialog.Builder(MainActivity.this);
+                    popup = getLayoutInflater().inflate(R.layout.result_popup, null);
+                    alerta = (TextView) popup.findViewById(R.id.resultadoTextView);
+                    alerta.setText("Por favor, insira peso e altura.");
+                    builder.setView(popup);
+                    dialog = builder.create();
+                    dialog.show();
                 } else {
                     // Declarando variáveis
                     double imc, ps, al;
@@ -49,9 +62,6 @@ public class MainActivity extends AppCompatActivity {
                     // Exibir resultado
                     resultado.setText("Seu IMC é " + ((double) Math.round(imc * 10)) / 10);
                 }
-                builder.setView(popup);
-                dialog = builder.create();
-                dialog.show();
             }
         });
     }
